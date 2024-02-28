69b4a3b8def91b101b4db05828f5369b74e888b3
Michel Hopman <michelhpmn@gmail.com>
2024-02-28 20:04:28 +0100

    second addition

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const links = [
+  {
+    category: "Open Source",
+    description: "Fullstack React framework",
+    id: 1,
+    imageUrl: "https://github.com/vercel.png",
+    title: "Next.js",
+    url: "https://nextjs.org",
+  },
+  {
+    category: "Open Source",
+    description: "Next Generation ORM for TypeScript and JavaScript",
+    id: 2,
+    imageUrl: "https://github.com/prisma.png",
+
+    title: "Prisma",
+    url: "https://prisma.io",
+  },
+  {
+    category: "Open Source",
+    description: "Utility-fist css framework",
+    id: 3,
+    imageUrl: "https://github.com/tailwindlabs.png",
+    title: "TailwindCSS",
+    url: "https://tailwindcss.com",
+  },
+  {
+    category: "Open Source",
+    description: "GraphQL implementation ",
+    id: 4,
+    imageUrl: "https://www.apollographql.com/apollo-home.jpg",
+    title: "Apollo GraphQL",
+    url: "https://apollographql.com",
+  },
+];
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: `testemail@gmail.com`,
+      role: "ADMIN",
+    },
+  });
+
+  await prisma.link.createMany({
+    data: links,
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
